462b3b662b95c239e4bc608f24e6a4f379d06830
talha.rizwan <talha.rizwan7170@hotmail.com>
2023-04-03 21:38:15 +0500

    Blogs added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+import Posts from "./components/Posts";
 
 export default function Home() {
   return (
     <main className="px-6 mx-auto">
-      <p className="mt-12 mb-12 text-3xl text-center dark:text-white" >Hello And Welcome 👋&nbsp;
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello And Welcome 👋&nbsp;
         <span className="whitespace-nowrap">
           I'm <span className="font-bold">Talha</span>
         </span>
       </p>
+      <Posts />
     </main>
-  )
+  );
 }
